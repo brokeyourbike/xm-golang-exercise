0137cc357cbf043832340598449e533040e3ef4d
Ivan Stasiuk <brokeyourbike@gmail.com>
2022-04-28 14:29:37 +0300

    fix: use proper responses

--- api/handlers/companies.go
+++ api/handlers/companies.go
@@ -16,8 +16,8 @@ type CompanyPayloadCtxKey struct{}
 // ErrResponse renderer type for handling all sorts of errors.
 type ErrResponse struct {
 	HTTPStatusCode int      `json:"-"`                // http response status code
-	Message        string   `json:"message"`          // user-level status message
-	Errors         []string `json:"errors,omitempty"` // application-level errors
+	Message        string   `json:"message"`          // status message
+	Errors         []string `json:"errors,omitempty"` // validation errors
 }
 
 func (e ErrResponse) Render(w http.ResponseWriter, r *http.Request) error {
@@ -84,10 +84,11 @@ func (c *companies) HandleCompanyUpdate(w http.ResponseWriter, r *http.Request) 
 	data.ID = company.ID
 
 	if err := c.companiesRepo.Update(data); err != nil {
+		render.Render(w, r, &ErrResponse{Message: "Cannot update company", HTTPStatusCode: http.StatusInternalServerError})
 		return
 	}
 
-	render.Render(w, r, &CompanyResponse{Company: &data, HTTPStatusCode: http.StatusCreated})
+	render.Render(w, r, &CompanyResponse{Company: &data, HTTPStatusCode: http.StatusOK})
 }
 
 // HandleCompanyDelete handles DELETE requests to delete companies
@@ -95,7 +96,7 @@ func (c *companies) HandleCompanyDelete(w http.ResponseWriter, r *http.Request) 
 	company := r.Context().Value(CompanyCtxKey{}).(models.Company)
 
 	if err := c.companiesRepo.Delete(company.ID); err != nil {
-		render.Render(w, r, &ErrResponse{Message: "Cannot removed company", HTTPStatusCode: http.StatusInternalServerError})
+		render.Render(w, r, &ErrResponse{Message: "Cannot remove company", HTTPStatusCode: http.StatusInternalServerError})
 		return
 	}
 
--- api/handlers/companies_test.go
+++ api/handlers/companies_test.go
@@ -1,51 +1,151 @@
 package handlers
 
 import (
-	"fmt"
+	"context"
+	"errors"
 	"net/http"
 	"net/http/httptest"
+	"strings"
 	"testing"
 
 	"github.com/brokeyourbike/xm-golang-exercise/api/server"
 	"github.com/brokeyourbike/xm-golang-exercise/mocks"
+	"github.com/brokeyourbike/xm-golang-exercise/models"
 	"github.com/go-chi/chi/v5"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
 )
 
-func TestHandleCompanyDelete(t *testing.T) {
+type dymmyMw struct{}
+
+func (d dymmyMw) Handle(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		next.ServeHTTP(w, r)
+	})
+}
+
+func TestCompanies(t *testing.T) {
 	cases := map[string]struct {
-		companyID  string
+		method     string
+		path       string
 		statusCode int
-		message    string
-		setupMock  func(companiesRepo *mocks.CompaniesRepo, mw *mocks.Middleware)
+		response   string
+		setupCtx   func(*http.Request) context.Context
+		setupMock  func(companiesRepo *mocks.CompaniesRepo)
 	}{
-		"username is required": {
-			companyID:  "10",
-			statusCode: http.StatusNotFound,
-			message:    "UserName is too short\n",
-			setupMock: func(companiesRepo *mocks.CompaniesRepo, mw *mocks.Middleware) {
-				mw.On("Handle", mock.Anything).Return(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {}))
+		"view company": {
+			method:     http.MethodGet,
+			path:       "/companies/30",
+			statusCode: http.StatusOK,
+			response:   `{"id":30,"name":"tes","code":"tt","country":"US","website":"example.com","phone":"+1234"}`,
+			setupCtx: func(req *http.Request) context.Context {
+				company := models.Company{ID: 30, Name: "tes", Code: "tt", Country: "US", Website: "example.com", Phone: "+1234"}
+				return context.WithValue(req.Context(), CompanyCtxKey{}, company)
+			},
+			setupMock: func(companiesRepo *mocks.CompaniesRepo) {},
+		},
+		"create company": {
+			method:     http.MethodPost,
+			path:       "/companies",
+			statusCode: http.StatusCreated,
+			response:   `{"id":1,"name":"tes","code":"123","country":"US","website":"example.com","phone":"+1234"}`,
+			setupCtx: func(req *http.Request) context.Context {
+				data := models.Company{Name: "tes", Code: "123", Country: "US", Website: "example.com", Phone: "+1234"}
+				return context.WithValue(req.Context(), CompanyPayloadCtxKey{}, data)
+			},
+			setupMock: func(companiesRepo *mocks.CompaniesRepo) {
+				company := models.Company{ID: 1, Name: "tes", Code: "123", Country: "US", Website: "example.com", Phone: "+1234"}
+				companiesRepo.On("Create", mock.AnythingOfType("Company")).Return(company, nil)
+			},
+		},
+		"company cannot be created": {
+			method:     http.MethodPost,
+			path:       "/companies",
+			statusCode: http.StatusInternalServerError,
+			response:   `{"message":"Company cannot be created"}`,
+			setupCtx: func(req *http.Request) context.Context {
+				return context.WithValue(req.Context(), CompanyPayloadCtxKey{}, models.Company{})
+			},
+			setupMock: func(companiesRepo *mocks.CompaniesRepo) {
+				companiesRepo.On("Create", mock.AnythingOfType("Company")).Return(models.Company{}, errors.New("cannot create"))
+			},
+		},
+		"cannot remove company": {
+			method:     http.MethodDelete,
+			path:       "/companies/30",
+			statusCode: http.StatusInternalServerError,
+			response:   `{"message":"Cannot remove company"}`,
+			setupCtx: func(req *http.Request) context.Context {
+				company := models.Company{ID: 30}
+				return context.WithValue(req.Context(), CompanyCtxKey{}, company)
+			},
+			setupMock: func(companiesRepo *mocks.CompaniesRepo) {
+				companiesRepo.On("Delete", uint64(30)).Return(errors.New("cannot remove"))
+			},
+		},
+		"company removed": {
+			method:     http.MethodDelete,
+			path:       "/companies/30",
+			statusCode: http.StatusOK,
+			response:   `{"message":"Company removed"}`,
+			setupCtx: func(req *http.Request) context.Context {
+				company := models.Company{ID: 30}
+				return context.WithValue(req.Context(), CompanyCtxKey{}, company)
+			},
+			setupMock: func(companiesRepo *mocks.CompaniesRepo) {
+				companiesRepo.On("Delete", uint64(30)).Return(nil)
+			},
+		},
+		"cannot update company": {
+			method:     http.MethodPut,
+			path:       "/companies/30",
+			statusCode: http.StatusInternalServerError,
+			response:   `{"message":"Cannot update company"}`,
+			setupCtx: func(req *http.Request) context.Context {
+				company := models.Company{ID: 30}
+				data := models.Company{}
+
+				ctx := context.WithValue(req.Context(), CompanyCtxKey{}, company)
+				return context.WithValue(ctx, CompanyPayloadCtxKey{}, data)
+			},
+			setupMock: func(companiesRepo *mocks.CompaniesRepo) {
+				companiesRepo.On("Update", mock.AnythingOfType("Company")).Return(errors.New("cannot update"))
+			},
+		},
+		"company updated": {
+			method:     http.MethodPut,
+			path:       "/companies/30",
+			statusCode: http.StatusOK,
+			response:   `{"id":30,"name":"after","code":"a123","country":"AU","website":"after.com","phone":"+56789"}`,
+			setupCtx: func(req *http.Request) context.Context {
+				company := models.Company{ID: 30, Name: "before", Code: "b123", Country: "BE", Website: "before.com", Phone: "+1234"}
+				data := models.Company{Name: "after", Code: "a123", Country: "AU", Website: "after.com", Phone: "+56789"}
+
+				ctx := context.WithValue(req.Context(), CompanyCtxKey{}, company)
+				return context.WithValue(ctx, CompanyPayloadCtxKey{}, data)
+			},
+			setupMock: func(companiesRepo *mocks.CompaniesRepo) {
+				companiesRepo.On("Update", mock.AnythingOfType("Company")).Return(nil)
 			},
 		},
 	}
 
 	for name, c := range cases {
 		t.Run(name, func(t *testing.T) {
-			t.Parallel()
-
 			companiesRepo := new(mocks.CompaniesRepo)
-			mw := new(mocks.Middleware)
 			companies := NewCompanies(companiesRepo)
-			c.setupMock(companiesRepo, mw)
+			c.setupMock(companiesRepo)
 
-			req := httptest.NewRequest(http.MethodPost, fmt.Sprintf("/user/%s", c.companyID), nil)
+			req := httptest.NewRequest(c.method, c.path, nil)
 			w := httptest.NewRecorder()
 
+			mw := dymmyMw{}
+
 			srv := server.NewServer(chi.NewMux(), companies, mw, mw, mw)
-			srv.ServeHTTP(w, req)
+			srv.ServeHTTP(w, req.WithContext(c.setupCtx(req)))
 
 			assert.Equal(t, c.statusCode, w.Result().StatusCode)
+			assert.Equal(t, c.response, strings.Trim(w.Body.String(), "\n"))
 
 			companiesRepo.AssertExpectations(t)
 		})
